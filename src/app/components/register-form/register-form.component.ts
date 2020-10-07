6babcad3269bdea4f9233ffa01470cc459caf4e5
Nguyen Ngoc Quynh <ngocquynh.ngoctam@gmail.com>
2020-10-07 10:00:47 +0700

    component create

---
+++ src/app/components/register-form/register-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-register-form',
+  templateUrl: './register-form.component.html',
+  styleUrls: ['./register-form.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class RegisterFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
