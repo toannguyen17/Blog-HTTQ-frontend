4ae01efaf7a5cdfde7ed048d6e65ada3ad99b015
nkdblack94 <nkdblack1294@gmail.com>
2020-10-07 10:14:16 +0700

    tạo giao diện thay đổi mật khẩu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChangePasswordComponent } from './change-password/change-password.component';
-
+import {ChangePasswordComponent} from './components/change-password/change-password.component';
 @NgModule({
   declarations: [
     AppComponent,
