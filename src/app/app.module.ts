680f10fc3402921e35a05551b1e4b68eaeedcc18
nkdblack94 <nkdblack1294@gmail.com>
2020-10-07 14:55:06 +0700

    tao profile template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ChangePasswordComponent} from './components/change-password/change-password.component';
+import { ProfileComponent } from './components/profile/profile.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ChangePasswordComponent
+    ChangePasswordComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
